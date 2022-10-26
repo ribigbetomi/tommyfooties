a13707f1ce97b777d31461d541ddcfbf29a2841d
Aderibigbe Adetomi <ribigbetomi@gmail.com>
2022-10-26 18:05:08 +0100

    ot

--- routes/paystack.js
+++ routes/paystack.js
@@ -4,19 +4,21 @@ const router = express.Router();
 const { Sale } = require("../models/sale");
 const crypto = require("crypto");
 const axios = require("axios");
+require("dotenv").config();
 
 router.get("/verify/:reference", async (req, res) => {
   let ref = req.params.reference;
-  const secret = process.env.SECRET_KEY;
 
   console.log(ref);
 
   let output;
 
+  const secret = process.env.SECRET_KEY;
+  console.log(secret);
   await axios
     .get(`https://api.paystack.co/transaction/verify/${ref}`, {
       headers: {
-        authorization: secret,
+        authorization: `Bearer ${secret}`,
         "content-type": "application/json",
         "cache-control": "no-cache",
       },
@@ -25,71 +27,74 @@ router.get("/verify/:reference", async (req, res) => {
       output = res;
     })
     .catch((error) => {
+      // output = error;
       output = error;
     });
+
   console.log(output);
 
-  if (output.data.data.status === "success")
-    res.send("Payment was successfully verified");
+  if (output.data.data.status === "success") res.send("Successful payment");
+
+  // res.status(200).send("Payment was successfully verified");
 });
 
-// router.post("/webhook", function (req, res) {
-//   const url = process.env.CLIENT_URL;
-//   let eventType;
-//   let event;
+router.post("/webhook", function (req, res) {
+  const url = process.env.CLIENT_URL;
+  let eventType;
+  let event;
 
-//   const hash = crypto
-//     .createHmac("sha512", secret)
-//     .update(JSON.stringify(req.body))
-//     .digest("hex");
+  const hash = crypto
+    .createHmac("sha512", secret)
+    .update(JSON.stringify(req.body))
+    .digest("hex");
 
-//   if (hash == req.headers["x-paystack-signature"]) {
-//     event = JSON.parse(req.body);
-//     console.log(event);
-//     eventType = event.event;
+  if (hash == req.headers["x-paystack-signature"]) {
+    event = JSON.parse(req.body);
+    console.log(event);
+    eventType = event.event;
 
-//     if (eventType === "charge.success") {
-//       const data = _.at(event.data, [
-//         "reference",
-//         "amount",
-//         "email",
-//         "first_name",
-//         "cart_items",
-//       ]);
+    if (eventType === "charge.success") {
+      const data = _.at(event.data, [
+        "reference",
+        "amount",
+        "email",
+        "first_name",
+        "cart_items",
+      ]);
 
-//       [reference, amount, email, first_name, cart_items] = data;
+      [reference, amount, email, first_name, cart_items] = data;
 
-//       newSale = {
-//         reference,
-//         amount,
-//         email,
-//         first_name,
-//         cart_items,
-//       };
+      newSale = {
+        reference,
+        amount,
+        email,
+        first_name,
+        cart_items,
+      };
 
-//       const sale = new Sale(newSale);
-//       sale
-//         .save()
-//         .then((sale) => {
-//           //   if (sale) {
-//           //     res.redirect("/receipt/" + reference);
-//           //   }
-//           console.log(sale);
-//         })
-//         .catch((e) => {
-//           res.redirect(`${url}/error`);
-//         });
-//     }
+      const sale = new Sale(newSale);
+      sale
+        .save()
+        .then((sale) => {
+          //   if (sale) {
+          //     res.redirect("/receipt/" + reference);
+          //   }
+          console.log(sale);
+        })
+        .catch((e) => {
+          res.redirect(`${url}/error`);
+        });
+    }
 
-//     // await stripe.customers
-//     //   .retrieve(data.customer)
-//     //   .then((customer) => {
-//     //     createOrder(customer, data);
-//     //   })
-//     //   .catch((err) => console.log(err.message));
-//   }
-//   res.send(200);
-// });
+    // await stripe.customers
+    //   .retrieve(data.customer)
+    //   .then((customer) => {
+    //     createOrder(customer, data);
+    //   })
+    //   .catch((err) => console.log(err.message));
+  }
+  res.send(200);
+});
 
 // router.post("/pay", (req, res) => {
 //   const form = _.pick(req.body, ["amount", "userEmail", "userName"]);
